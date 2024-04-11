603bfe16497ff425a26da5b430fd3c2267a6caeb
yue <2794127100@qq.com>
2024-04-11 23:17:22 +0800

    按钮

--- flutter01/lib/main.dart
+++ flutter01/lib/main.dart
@@ -26,74 +26,65 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-  List<Widget> initListData() {
-    List<Widget> list = [];
-    for (var i = 0; i < 20; i++) {
-      //   list.add(const Card(
-      //     shape: RoundedRectangleBorder(
-      //       borderRadius: BorderRadius.all(Radius.circular(10)),
-      //     ),
-      //     elevation: 10,
-      //     child: Column(
-      //       children: [
-      //         ListTile(
-      //           title: Text(
-      //             "张三",
-      //             style: TextStyle(fontSize: 28),
-      //           ),
-      //           subtitle: Text("前端开发工程师"),
-      //         ),
-      //         Divider(),
-      //         ListTile(
-      //           leading: Icon(Icons.phone_android),
-      //           title: Text("电话: 1234567890"),
-      //           subtitle: Text("地址: 杭州市西湖区"),
-      //         )
-      //       ],
-      //     ),
-      //   ));
-
-      list.add(Card(
-        margin: const EdgeInsets.all(10),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        elevation: 10,
-        child: Column(
-          children: [
-            AspectRatio(
-              aspectRatio: 16 / 7,
-              child: Image.network(
-                  'https://img0.baidu.com/it/u=2067833172,1642959919&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712854800&t=b3989401815a7d6da05d80e3f4cafe81',
-                  fit: BoxFit.cover),
-            ),
-            const ListTile(
-              // leading: ClipOval(
-              //   child: Image.network(
-              //     'https://img0.baidu.com/it/u=2067833172,1642959919&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712854800&t=b3989401815a7d6da05d80e3f4cafe81',
-              //     fit: BoxFit.cover,
-              //     height: 40,
-              //     width: 40,
-              //   ),
-              // ),
-              leading: CircleAvatar(backgroundImage: NetworkImage('https://img0.baidu.com/it/u=2067833172,1642959919&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712854800&t=b3989401815a7d6da05d80e3f4cafe81'),),
-              title: Text(
-                "xxx",
-              ),
-              subtitle: Text("xxxxxxxxx"),
-            )
-          ],
-        ),
-      ));
-    }
-    return list;
-  }
 
   @override
   Widget build(BuildContext context) {
     return ListView(
-      padding: const EdgeInsets.all(10),
-      children: initListData(),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('普通按钮')),
+            TextButton(onPressed: () {}, child: const Text('文本按钮')),
+            const OutlinedButton(onPressed: null, child: Text('边框按钮')),
+            const IconButton(
+              onPressed: null,
+              icon: Icon(Icons.home),
+            ),
+            ElevatedButton.icon(
+              onPressed: null,
+              icon: const Icon(Icons.home),
+              label: const Text('图标按钮'),
+            ),
+            TextButton.icon(
+              onPressed: null,
+              icon: const Icon(Icons.home),
+              label: const Text('图标按钮'),
+            ),
+            OutlinedButton.icon(
+              onPressed: null,
+              icon: const Icon(Icons.home),
+              label: const Text('图标按钮'),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              height: 60,
+              width: 140,
+              child: const ElevatedButton(
+                onPressed: null,
+                child: Text('大按钮'),
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              width: 60,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(const CircleBorder(
+                    side: BorderSide(width: 1, color: Colors.red),
+                  )),
+                ),
+                onPressed: () {},
+                child: const Text('圆形'),
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
