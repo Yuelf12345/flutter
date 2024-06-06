44bd428a953b5f5e28594613006af2ef0eaea20c
yue <2794127100@qq.com>
2024-06-06 21:54:56 +0800

    feat: 侧边栏和顶部栏

--- flutter01/lib/main14(左侧菜单).dart
+++ flutter01/lib/main14(左侧菜单).dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
@@ -35,6 +37,54 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Demo Home Page'),
+      ),
+      drawer: const Drawer(
+          child: Column(
+        children: [
+          Row(
+            children: [
+              // UserInfo()
+              Expanded(
+                  flex: 1,
+                  child: UserAccountsDrawerHeader(
+                      accountName: Text(
+                        "Deja Vu",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      accountEmail: Text(
+                        "2794127100@qq.com",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      otherAccountsPictures: [
+                        Image(image: NetworkImage('https://img0.baidu.com/it/u=2067833172,1642959919&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712854800&t=b3989401815a7d6da05d80e3f4cafe81'))
+                      ],
+                      currentAccountPicture: CircleAvatar(
+                        backgroundImage: NetworkImage('https://img0.baidu.com/it/u=2067833172,1642959919&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712854800&t=b3989401815a7d6da05d80e3f4cafe81'),
+                      ),
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://img0.baidu.com/it/u=2067833172,1642959919&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712854800&t=b3989401815a7d6da05d80e3f4cafe81')))))
+            ],
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              child: Icon(Icons.person),
+            ),
+            title: Text('Personal Center'),
+          ),
+          Divider(),
+          ListTile(
+            leading: CircleAvatar(
+              child: Icon(Icons.settings),
+            ),
+            title: Text('System settings'),
+          ),
+        ],
+      )),
+      endDrawer: const Drawer(
+        child: Text('right icon'),
       ),
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
@@ -79,6 +129,37 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButtonLocation:
           FloatingActionButtonLocation.centerDocked, // 浮动按钮位置
+    );
+  }
+}
+
+class UserInfo extends StatelessWidget {
+  const UserInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(
+      child: DrawerHeader(
+          decoration: BoxDecoration(
+              // color: Colors.blue,
+              image: DecorationImage(
+                  image: NetworkImage(
+                      "https://img0.baidu.com/it/u=2067833172,1642959919&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712854800&t=b3989401815a7d6da05d80e3f4cafe81"),
+                  fit: BoxFit.cover)),
+          child: Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://img0.baidu.com/it/u=2067833172,1642959919&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712854800&t=b3989401815a7d6da05d80e3f4cafe81'),
+                ),
+                title: Text(
+                  'Deja Vu',
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
